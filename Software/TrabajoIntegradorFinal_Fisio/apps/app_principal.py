a88940d012ca2e150be80811d9388ebfc3d77418
irinalauritto <irina.lauritto@ingenieria.uner.edu.ar>
2025-05-09 18:25:32 -0300

    se muestra imagen difuminada

--- Software/TrabajoIntegradorFinal_Fisio/apps/app_principal.py
+++ Software/TrabajoIntegradorFinal_Fisio/apps/app_principal.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import font as tkFont
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageFilter
 import modules.GestorDeArchivos as ga
 import modules.GestorDeImagenes as gi
 import modules.MarchaDeRayos as mr
@@ -15,6 +15,7 @@ gArchivos.extraeListadoDeArchivos("\\RepositorioTIF\\Software\\TrabajoIntegrador
 directoriosImagenes = gArchivos.getListadoDeArchivos()
 gImagen = gi.gestorDeImagenes("Gestor de Imagenes")
 mRayos = mr.MarchaDeRayos("Marcha de Rayos", 250, 25, 1000)
+gradoDeDifuminado = 2  # Grado de difuminado inicial
 
 # Imprimir los archivos cargados para verificar
 print("Archivos cargados desde el directorio:")
@@ -28,8 +29,11 @@ class AplicacionPrincipal:
         self.root.title("Simulador del ojo humano")
         self.root.state('zoomed')  # Pantalla completa
 
-        # Crear la barra de tareas
-        self.frameBarraTareas = tk.Frame(self.root)
+        # Cambiar el color de fondo de la ventana principal
+        self.root.configure(bg="#c9c9c9")
+
+        # Crear la barra de tareas con fondo personalizado
+        self.frameBarraTareas = tk.Frame(self.root, bg="#c9c9c9")
         self.frameBarraTareas.pack(fill='x')
 
         # Botón para seleccionar imágenes
@@ -37,15 +41,15 @@ class AplicacionPrincipal:
         self.imagenSeleccionada = tk.StringVar(root)
         self.imagenSeleccionada.set("Seleccionar imagen")
 
-        menu_button_imagen = tk.Menubutton(self.frameBarraTareas, text="Seleccionar Imagen", relief=tk.RAISED)
+        menu_button_imagen = tk.Menubutton(self.frameBarraTareas, text="Seleccionar Imagen", relief=tk.RAISED, bg="white")
         menu_button_imagen.menu = tk.Menu(menu_button_imagen, tearoff=0)
         menu_button_imagen["menu"] = menu_button_imagen.menu
-       
+
         for i, imagen in enumerate(imagenes):
-            menu_button_imagen.menu.add_radiobutton(label=imagen, variable=self.imagenSeleccionada, value=imagen, command=lambda i=i: self.mostrar_imagen(i))
+            menu_button_imagen.menu.add_radiobutton(label=imagen, variable=self.imagenSeleccionada, value=imagen, command=lambda i=i: self.mostrarImagenes(i, gradoDeDifuminado))
 
         menu_button_imagen.pack(side='left', padx=5, pady=5)
-        
+
         # Botón "Preguntas"
         preguntasBoton = tk.Button(self.frameBarraTareas, text="Preguntas", command=lambda: messagebox.showinfo("Preguntas", "Aquí van las preguntas."), relief=tk.RAISED)
         preguntasBoton.pack(side='left', padx=5, pady=5)
@@ -58,8 +62,8 @@ class AplicacionPrincipal:
         ImagenesPruebaBoton = tk.Button(self.frameBarraTareas, text="Imágenes de Prueba", command=lambda: messagebox.showinfo("Imágenes de Prueba", "Aquí van las imágenes de prueba."), relief=tk.RAISED)
         ImagenesPruebaBoton.pack(side='left', padx=5, pady=5)
 
-        # Grid principal
-        self.frameGrid = tk.Frame(root)
+        # Grid principal con fondo personalizado
+        self.frameGrid = tk.Frame(self.root, bg="#c9c9c9")
         self.frameGrid.pack(pady=10, expand=True, fill='both')
 
         # Configurar la expansión de las filas y columnas
@@ -68,8 +72,8 @@ class AplicacionPrincipal:
         self.frameGrid.columnconfigure(2, weight=1)
         self.frameGrid.rowconfigure(0, weight=1)
 
-        # Condición
-        self.frameCondicion = tk.Frame(self.frameGrid)
+        # Condición con fondo personalizado
+        self.frameCondicion = tk.Frame(self.frameGrid, bg="#c9c9c9")
         self.frameCondicion.grid(row=0, column=0, padx=10, sticky='nsew')
 
         condiciones = ["Emétrope", "Miope", "Hipermétrope"]
@@ -83,15 +87,15 @@ class AplicacionPrincipal:
         for condicion in condiciones:
             menuBotonCondicion.menu.add_radiobutton(label=condicion, variable=condicionSeleccionada, value=condicion)
 
-        menuBotonCondicion.pack(pady=5)
+        menuBotonCondicion.pack(pady=4)
 
         # Cuadro gris debajo de "Condicion"
-        self.imagenOriginal = ImageTk.PhotoImage(Image.new("RGB", (200, 200), "gray"))
-        self.labelImagenOriginal = tk.Label(self.frameCondicion, image=self.imagenOriginal)
-        self.labelImagenOriginal.pack(expand=True, fill='both')
+        self.imagenOriginal = ImageTk.PhotoImage(Image.new("RGB", (300, 300), "gray"))
+        self.labelImagenOriginal = tk.Label(self.frameCondicion, image=self.imagenOriginal, bg="#c9c9c9")
+        self.labelImagenOriginal.pack(expand=True, fill='both', padx=10, pady=10)
 
-        # Grado
-        self.frameGrado = tk.Frame(self.frameGrid)
+        # Grado con fondo personalizado
+        self.frameGrado = tk.Frame(self.frameGrid, bg="#c9c9c9")
         self.frameGrado.grid(row=0, column=1, padx=20, sticky='nsew')
 
         grados = ["Grado 1", "Grado 2", "Grado 3"]
@@ -110,16 +114,15 @@ class AplicacionPrincipal:
         canvas = tkagg.FigureCanvasTkAgg(fig, master=self.frameGrado)  # Integra el gráfico en Tkinter
 
         # Usa grid para posicionar el canvas
-        canvas.get_tk_widget().pack(pady = 50)  # Posiciona el gráfico en la cuadrícula
+        canvas.get_tk_widget().pack(pady = 100)  # Posiciona el gráfico en la cuadrícula
 
-   
 
         # Dibujar la simulación inicial
         mRayos.dibujarSimulacion(ax)  # Dibuja la simulación inicial
         #canvas.draw()
 
-        # Distancia
-        self.frameDistancia = tk.Frame(self.frameGrid)
+        # Distancia con fondo personalizado
+        self.frameDistancia = tk.Frame(self.frameGrid, bg="#c9c9c9")
         self.frameDistancia.grid(row=0, column=2, padx=10, sticky='nsew')
 
         distancias = ["Punto lejano", "Punto cercano", "Punto medio"]
@@ -136,8 +139,10 @@ class AplicacionPrincipal:
         menuBotonDistancia.pack(pady=5)
 
         # Cuadro gris debajo de "Distancia"
-        self.imagenEnElCerebro = ImageTk.PhotoImage(Image.new("RGB", (200, 200), "gray"))
-        tk.Label(self.frameDistancia, image=self.imagenEnElCerebro).pack(expand=True, fill='both')
+        self.imagenDifuminada = ImageTk.PhotoImage(Image.new("RGB", (300, 300), "gray"))
+        self.labelImagenDifuminada = tk.Label(self.frameDistancia, image=self.imagenDifuminada, bg="#c9c9c9")
+        self.labelImagenDifuminada.pack(expand=True, fill='both', padx=10, pady=10)
+
         # Información de distancia y botón
         frameInfo = tk.Frame(root, bd=2, relief=tk.SUNKEN, padx=35, pady=15, bg="#e0e0e0")
         frameInfo.pack(padx=10, pady=5, anchor='w')  # Align to the left
@@ -147,18 +152,35 @@ class AplicacionPrincipal:
         tk.Label(frameInfo, text="Punto Lejano:", bg="#e0e0e0").pack(anchor='w')
         tk.Label(frameInfo, text="Lente correctora:", bg="#e0e0e0").pack(anchor='w')
 
-    
-    def mostrar_imagen(self, index):
+   
+
+    def mostrarImagen(self, index):
         if index < len(directoriosImagenes):
             print(f"Mostrando imagen {index}")
             imagen_path = "\\RepositorioTIF\\Software\\TrabajoIntegradorFinal_Fisio\\imagenes_nuevas\\" + directoriosImagenes[index]
             print(f"Ruta de la imagen: {imagen_path}")
             imagen = gImagen.mostrar_imagen(imagen_path)
-            imagen = imagen.resize((200, 200))
+            imagen = imagen.resize((300, 300))
             self.imagenOriginal = ImageTk.PhotoImage(imagen)
             self.labelImagenOriginal.config(image=self.imagenOriginal)
             self.labelImagenOriginal.image = self.imagenOriginal
 
+    def mostrarImagenDifuminada(self, index, grado):
+            if index < len(directoriosImagenes):
+                print(f"Mostrando imagen {index}")
+                imagen_path = "\\RepositorioTIF\\Software\\TrabajoIntegradorFinal_Fisio\\imagenes_nuevas\\" + directoriosImagenes[index]
+                print(f"Ruta de la imagen: {imagen_path}")
+                imagen = gImagen.mostrar_imagen(imagen_path)
+                imagen = imagen.resize((300, 300))
+                imagenDifuminada = imagen.filter(ImageFilter.GaussianBlur(radius=grado))
+                self.imagenDifuminada = ImageTk.PhotoImage(imagenDifuminada)
+                self.labelImagenDifuminada.config(image=self.imagenDifuminada)
+                self.labelImagenDifuminada.image = self.imagenDifuminada
+
+    def mostrarImagenes(self, index, grado):
+        self.mostrarImagen(index)
+        self.mostrarImagenDifuminada(index, grado)
+        
 def ejecutar_gui():
     root = tk.Tk()
     app = AplicacionPrincipal(root)
